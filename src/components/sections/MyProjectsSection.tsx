0e47505c7defaf49a74ca54746b05d013bae31b6
SergeCodeur <amoussougboserge@gmail.com>
2024-02-19 09:06:34 +0100

    refactor: changement d'image

--- src/components/sections/MyProjectsSection.tsx
+++ src/components/sections/MyProjectsSection.tsx
@@ -1,6 +1,6 @@
-import Project1 from "@/assets/images/projet-img-1.svg";
-import Project2 from "@/assets/images/projet-img-2.svg";
-import Project3 from "@/assets/images/projet-img-3.svg";
+import Project1 from "@/assets/images/image-1.png";
+import Project2 from "@/assets/images/image-2.png";
+import Project3 from "@/assets/images/image-3.png";
 import MaxContentWidthWrapper from "../MaxContentWidthWrapper";
 import Title from "../ui/Title";
 import { ProjectCard, ProjectCardProps } from "../ui/card";
@@ -8,7 +8,7 @@ import { ProjectCard, ProjectCardProps } from "../ui/card";
 const MyProjects = [
   {
     project_name: "Crypto Screener Application",
-    project_image: Project1,
+    project_image: Project1.src,
     project_number: "01",
     project_link: "#",
     project_description:
@@ -16,7 +16,7 @@ const MyProjects = [
   },
   {
     project_name: "Euphoria - Ecommerce (Apparels) Website Template",
-    project_image: Project2,
+    project_image: Project2.src,
     project_number: "02",
     project_link: "#",
     project_description:
@@ -25,7 +25,7 @@ const MyProjects = [
   },
   {
     project_name: "Blog Website Template",
-    project_image: Project3,
+    project_image: Project3.src,
     project_number: "03",
     project_link: "#",
     project_description:
@@ -36,14 +36,14 @@ const MyProjects = [
 const MyProjectsSection = () => {
   return (
     <section className="bg-black" id="project">
-      <MaxContentWidthWrapper>
+      <MaxContentWidthWrapper className="space-y-5">
         <Title className="w-full my-5 text-white">
           <span className="lg:text-display-text text-display-text-m">My</span>
           <span className="lg:text-display-text-800 text-display-text-m-800">
             Projects
           </span>
         </Title>
-        <div className="space-y-5">
+        <div className="space-y-5 py-5">
           {MyProjects.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
