43b242c74a40421b8a2a142657e2dbad05137ccf
SergeCodeur <amoussougboserge@gmail.com>
2024-02-19 01:52:10 +0100

    feat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "@/components/sections/AboutSection";
+import ContactSection from "@/components/sections/ContactSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
 import Header from "@/components/sections/Header";
 import HeroBanner from "@/components/sections/HeroBanner";
 import MyProjectsSection from "@/components/sections/MyProjectsSection";
 import SkillsSection from "@/components/sections/SkillsSection";
+import TextimonialSection from "@/components/sections/TextimonialSection";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <ExperienceSection />
       <AboutSection />
       <MyProjectsSection />
+      <TextimonialSection />
+      <ContactSection />
     </main>
   );
 }
